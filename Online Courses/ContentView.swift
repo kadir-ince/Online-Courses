fb502038af360da34be95c0a7e032a314c9dfa4b
Kadir Ince <kadirince@pm.me>
2021-07-07 21:49:19 +0300

    Initial Commit

---
+++ Online Courses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Online Courses
+//
+//  Created by Kadir Ince on 7.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Online Courses/Online_CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_CoursesApp.swift
+//  Online Courses
+//
+//  Created by Kadir Ince on 7.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Online_CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
